f368270aef6eee034efe6eeae1a607f3d5775bea
Paul Panchukov <panchukovp@gmail.com>
2019-09-20 17:28:55 -0400

    set an offset for every time submit is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   let APIkey = "&api_key=WFWKuEi7c2nOXtzy42TUXMkcLKkB0jsS"
-
+var num = 0;
   $("#submitItem").on("click", function(e){
+    num = 0
     e.preventDefault()
     query = $("#addItem").val()
     if(query !== ""){
@@ -12,25 +13,28 @@ $(document).ready(function(){
       btns.text(query)
       $("#addItem").val("")
     }
+
   })
   $(document).on("click", ".btns", function(){
     $("#gifHolder").text("")
+    let offset = "&offset=" + num
     query = $(this).val()  
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q="+query+APIkey+"&limit=10"
+    let queryUrl = "https://api.giphy.com/v1/gifs/search?q="+query+APIkey+"&limit=10"+offset
           $.ajax({
               url: queryUrl,
               method: "GET"
           }).then(function(response){
-            for(var i = 0; i < response.data.length; i++) {
-            var imgsrc = response.data[i].images.downsized_medium.url
-            var gifs = $("<img>")
+            for(let i = 0; i < response.data.length; i++) {
+            let imgsrc = response.data[i].images.downsized_medium.url
+            let gifs = $("<img>")
             gifs.attr("id", query)
             gifs.attr("src", imgsrc)
             .width("250px")
             $("#gifHolder").prepend(gifs)
           }
-            console.log(response.data)
           });
+          num+= 10;
+        
       })
   
   });
